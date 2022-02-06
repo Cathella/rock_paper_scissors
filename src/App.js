9f2a77e9b0db32ca99a6263ad3034b2e5568c71d
Cathella <ncathie214@gmail.com>
2022-02-06 09:33:22 +0300

    Game setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+import Rock from './icons/Rock';
+import Paper from './icons/Paper';
+import Scissors from './icons/Scissors';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* game info here */}
+      <div className="info">
+        <h2>Rock. Paper. Scissors</h2>
+
+        {/* stats here */}
+        <div className="wins-losses">
+          <div className="wins">
+            <span className="number">0</span>
+            <span className="text">Wins</span>
+          </div>
+
+          <div className="losses">
+            <span className="number">0</span>
+            <span className="text">Losses</span>
+          </div>
+        </div>
+      </div>
+
+      {/* game state popup */}
+      <div className="game-state"></div>
+
+      <div className="choices">
+        {/* captions */}
+        <div>You</div>
+        <div />
+        <div>Computer</div>
+
+        {/* buttons */}
+        <div>
+          <button className="rock">
+            <Rock />
+          </button>
+          <button>
+            <Paper />
+          </button>
+          <button>
+            <Scissors />
+          </button>
+        </div>
+
+        <div className="vs">vs</div>
+
+        {/* computer choice */}
+        <div>
+          <button className="computer-choice">?</button>
+        </div>
+      </div>
     </div>
   );
 }
